6d3f57518738bb2c209509572825465775cdadd1
zcodeSolid <fezzazzakaria63@gmail.com>
2024-12-12 23:03:19 +0100

    installing phpcs fixer

--- src/Controller/main.php
+++ src/Controller/main.php
@@ -1,17 +1,15 @@
 <?php
 
 namespace App\Controller;
-use Symfony\Component\Routing\Attribute\Route;
+
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
-class main{
-  #[Route('/')]
-public function home(){
-
-  return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
-
-
-}
-
-
+class main
+{
+    #[Route('/')]
+    public function home()
+    {
+        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
+    }
 }
